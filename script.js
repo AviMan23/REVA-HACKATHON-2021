fe93533b2552017524434e91fb0d2a1b4e186b05
Anudeep S <anudeep313@gmail.com>
2021-11-12 23:38:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function generateQR(){
 let size="1000x1000";
 let data = qrtext.value;
 let baseURL= " http://api.qrserver.com/v1/create-qr-code/";
-let url = `${baseURL}?data=${data}&amp;size=${size}`;
+let url = `${baseURL}?data=${data}&size=${size}`;
 qrcode.src = url;
 }
 
