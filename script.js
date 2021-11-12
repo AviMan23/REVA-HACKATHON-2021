8aaa7a64314f74de7b2dabad955a12db668a5158
Anudeep S <anudeep313@gmail.com>
2021-11-12 23:56:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ let qrcode = select("img") ;
 let qrtext = select("textarea") ;
 let qrbtn = select("button");
 
-qrbtn.addEventListener('click', generateQR);
+qrbtn.addEventListener("click", generateQR);
 
 function generateQR(){
-let size="1000x1000";
-let data = qrtext.value;
-let baseURL= " http://api.qrserver.com/v1/create-qr-code/";
-let url = `${baseURL}?data=${data}&size=${size}`;
-qrcode.src = url;
+ let size="1000x1000";
+ let data = qrtext.value;
+ let baseURL= " http://api.qrserver.com/v1/create-qr-code/";
+ let url = `${baseURL}?data=${data}&size=${size}`;
+ return url;
 }
 
 function select(el) {
